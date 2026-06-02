8e5be84a415fc4ba36482155e2f6636258dfd11f
Olena <133535240+Olena1984@users.noreply.github.com>
2026-06-02 18:30:52 +0300

    Add Lesson_17

---
+++ Lesson_17/javascript/task1.js
@@ -0,0 +1,48 @@
+"use strict";
+// Створити клас, що дозволяє виконувати такі операції над масивами: знаходження кількості додатних, кількості від’ємних, кількість входжень деякого числа (статичні методи)
+class OperationsOnArrays {
+    arrNumbers;
+    static positiveNumbers = 0;
+    static negativeNumbers = 0;
+    static countNumbers = 0;
+    static searchNumber;
+    //   static searchNumber:number|null = null
+    constructor(arrNumbers) {
+        this.arrNumbers = arrNumbers;
+    }
+    static getPositiveNumbers(arr) {
+        for (const element of arr) {
+            if (element > 0) {
+                OperationsOnArrays.positiveNumbers += 1;
+            }
+        }
+        return OperationsOnArrays.positiveNumbers;
+    }
+    static getNegativeNumbers(arr) {
+        for (const element of arr) {
+            if (element < 0) {
+                OperationsOnArrays.negativeNumbers += 1;
+            }
+        }
+        return OperationsOnArrays.negativeNumbers;
+    }
+    static countSomeNumber(arr, searchNumber) {
+        OperationsOnArrays.searchNumber = searchNumber;
+        for (const element of arr) {
+            if (element === OperationsOnArrays.searchNumber) {
+                OperationsOnArrays.countNumbers += 1;
+            }
+        }
+        return OperationsOnArrays.countNumbers;
+    }
+    toString() {
+        return `кількість додатних чисел = ${OperationsOnArrays.positiveNumbers}<br>кількість від’ємних чисел = ${OperationsOnArrays.negativeNumbers}<br>кількість входжень числа ${OperationsOnArrays.searchNumber}  = ${OperationsOnArrays.countNumbers}`;
+    }
+}
+const arr = [23, -676, -1, 98, 456, 3, -56, 98, 1, 5, 5, 23];
+const operations = new OperationsOnArrays(arr);
+console.log(OperationsOnArrays.getPositiveNumbers(arr));
+console.log(OperationsOnArrays.getNegativeNumbers(arr));
+console.log(OperationsOnArrays.countSomeNumber(arr, 5));
+document.write(`<p>${operations.toString()}</p>`);
+//# sourceMappingURL=task1.js.map
---
+++ Lesson_17/javascript/task2.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+Створити службове авто (водій, марка, номер). Створити клас
+таким чином, щоб можна було створити тільки один екземпляр
+цього класу.
+*/
+class Car {
+    static companyCar;
+    name = '';
+    brand = '';
+    carNumber = '';
+    constructor(name, brand, carNumber) {
+        if (Car.companyCar)
+            return Car.companyCar;
+        this.name = name;
+        this.brand = brand;
+        this.carNumber = carNumber;
+        Car.companyCar = this;
+    }
+    toString() {
+        return `Службове авто:<br> водій - ${this.name}<br> марка- ${this.brand}<br> номерний знак- ${this.carNumber}`;
+    }
+}
+const result = new Car('Stasiv Petro', 'Skoda', 'ВС2345АА');
+document.write(`<p> ${result.toString()}</p>`);
+const result2 = new Car('Onysko Bohdan', 'Renault', 'ВС4222АА');
+document.write(`<p> ${result.toString()}</p>`);
+const result3 = new Car('Onysko Olena', 'Kia', 'ВС5555АB');
+document.write(`<p> ${result.toString()}</p>`);
+//# sourceMappingURL=task2.js.map
